a68b6d3a7ed6ae23cd7b0b221075c7d3bd44bb04
Anh Le Hoang <lehoanganh25991@gmail.com>
2020-05-15 13:48:24 +0700

    [rename] Update

--- format-file-name-in-dir/index.js
+++ format-file-name-in-dir/index.js
@@ -1,51 +1,38 @@
-var fs = require('fs');
+const fs = require("fs");
 
-console.log(process.argv[2]);
+const directory = process.argv[2];
+console.log("[dirname]", directory);
 
-var formatFilename = function (s){
-	//trim s, remove space begin&end
-	s = s.trim();
+const formatFilename = function (s) {
+  //trim s, remove space begin&end
+  s = s.trim();
 
-	//turn into -
-	s = s.replace(/(\s|_|\+)+/g, '-');
+  //turn into -
+  s = s.replace(/(\s|_|\+)+/g, "-");
 
-	//only one - form multiple ---
-	s = s.replace(/-+/g, '-');
+  //only one - form multiple ---
+  s = s.replace(/-+/g, "-");
 
-	//convert to lower
-	s = s.toLowerCase();
+  //convert to lower
+  s = s.toLowerCase();
 
-	return s;
-}
+  //remove microsoft-teams
+  s = s.replace("-microsoft-teams", "");
 
-// clipboardHandler.paste(function(err, dirname){
-// 	console.log(dirname);
-// 	fs.readdir(dirname, function(err, items){
-// 		var p = /^~\$|^\.|^\../;
-// 		items.forEach(function(item){
-// 			if(!p.test(item)){
-// 				fs.rename(item, formatFilename(item), function(err){
-// 					if(err) throw err;
-// 					console.log('%s => \033[01;32m%s\033[0m', item, formatFilename(item));
-// 				});
-// 			}
-// 		});
-// 	});
-// });
-var dirname = process.argv[2];
-fs.readdir(dirname, function(err, items){
-	var p = /^~\$|^\.|^\../;
-	items.forEach(function(item){
-		var newName = formatFilename(item);
-		console.log(item == newName);
-		if(!p.test(item) && item != newName){
-			fs.rename(dirname + "/" + item, dirname + "/" + newName, function(err){
-				if(err){
-					console.log('\033[01;31m[E]\033[0mError at %s', item);
-					return;
-				}
-				console.log('%s => \033[01;32m%s\033[0m', item, newName);
-			});
-		}
-	});
+  return s;
+};
+
+fs.readdir(directory, function (err, items) {
+  const p = /^~\$|^\.|^\../;
+
+  items.forEach(function (item) {
+    const newName = formatFilename(item);
+    console.log(item == newName);
+
+    if (!p.test(item) && item != newName) {
+      const oldPath = directory + "/" + item;
+      const newPath = directory + "/" + newName;
+      fs.rename(oldPath, newPath, console.log);
+    }
+  });
 });
